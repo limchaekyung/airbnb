998ef409143f7458d964d4a2d4503a7a39f653c1
limchaekyung <corud505@naver.com>
2022-04-09 23:58:38 +0900

    setup

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+      <h1>Hey Clever Programmer, Let's build AIRBNB clone 😋!</h1>
+    </div>
+
+    // Home
+    //      Header
+
+    //      Banner
+    //          Search
+
+    //      Cards
+
+    //      Footer
+
+    // Search Page
+    // ...
+
+  );
+}
+
+export default App;
