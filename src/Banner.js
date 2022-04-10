4727fa673bf885ac19e502add1bb248c8ace72df
limchaekyung <corud505@naver.com>
2022-04-10 22:21:55 +0900

    navigator

--- src/Banner.js
+++ src/Banner.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 import './Banner.css'
 import { Button } from "@material-ui/core";
 import Search from './Search'
+import { useNavigate } from "react-router-dom";
 
 function Banner() {
+    const navigate = useNavigate();
     const [showSearch, setShowSearch] = useState(false);
 
     return (
@@ -20,7 +22,7 @@ function Banner() {
                 <h5>
                     Plan a different kind of getaway to uncover the hidden gems near you.
                 </h5>
-                <Button variant='outlined'>Explore Nearby</Button>
+                <Button variant='outlined' onClick={() => navigate('/search')}>Explore Nearby</Button>
             </div>
         </div>
     )
