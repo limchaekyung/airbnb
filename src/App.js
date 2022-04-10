105d0b1a92c9006cf3b68ff09adc6124c6a72578
limchaekyung <corud505@naver.com>
2022-04-10 22:05:04 +0900

    search page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
+import React from "react";
 import './App.css';
 import Home from './Home'
 import Header from './Header'
 import Footer from './Footer'
+import SearchPage from './SearchPage'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
 
     // BEM
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
-
-      {/* Home */}
-      {/*   Header */}
-
-      {/*   Banner */}
-      {/*     Search */}
-
-      {/*   Cards */}
-
-      {/*   Footer */}
-
-      {/* Search Page */}
-      {/*   Header */}
-      {/*   ... */}
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 
